c89b98b2b5dd3a45ba6904503d8e90015372d629
waynexiao567890 <13671898587@163.com>
2019-04-15 15:41:13 +0800

    合同界面参考tobe增加了一些字段

--- aop_sale/models/delivery_carrier.py
+++ aop_sale/models/delivery_carrier.py
@@ -13,3 +13,47 @@ class DeliveryCarrier(models.Model):
 
     aop_route_ids = fields.Many2many('aop.route', string='AOP route')
 
+    serial_number = fields.Char(string='合同编号')
+
+    version_id = fields.Many2one('contract.version', string="版本")
+
+    serial_no = fields.Char(string='合同号')
+
+    is_formal = fields.Boolean(string='正式合同', default=True)
+
+    project_id = fields.Many2one('contract.project', string="项目")
+
+    date_start = fields.Date('合同开始时间', required=True, default=fields.Date.today)
+
+    date_end = fields.Date('合同结束时间')
+
+    effective_date = fields.Date('合同生效日')
+
+    expiry_date = fields.Date('合同失效日')
+
+    source = fields.Char(string='合同来源')
+
+    type = fields.Selection(
+        [
+            ('buyer', '需方合同'),
+            ('supplier', '供方合同')
+        ],
+        string='合同类型',
+        default='buyer')
+
+
+
+
+
+
+
+class ContractVersion(models.Model):
+    _name = 'contract.version'
+
+    name = fields.Char(string='版本')
+
+
+class ContractProject(models.Model):
+    _name = 'contract.project'
+
+    name = fields.Char(string='项目')
