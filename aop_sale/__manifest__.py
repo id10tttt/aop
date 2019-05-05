dacf1a6879797283e5506f9e8556d8af26ecabd5
waynexiao567890 <13671898587@163.com>
2019-05-05 16:11:36 +0800

    物流网络菜单指向原库房模块，并新增保险管理和质损管理菜单。手动增加的物流网络菜单更名为物流网络 (功能矩阵)，并移到"其它"菜单下

--- aop_sale/__manifest__.py
+++ aop_sale/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale', 'sale_management', 'sale_stock', 'delivery', 'stock_picking_batch', 'purchase_stock', 'barcodes'],
+    'depends': ['account', 'sale', 'sale_management', 'sale_stock', 'delivery', 'stock_picking_batch', 'purchase_stock', 'barcodes'],
 
     # always loaded
     'data': [
