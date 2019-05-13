85db7eb94f9f7b4df294358cd5fe01b9c98296ad
waynexiao567890 <13671898587@163.com>
2019-05-13 15:02:58 +0800

    1

--- aop_sale/models/sale_order_line.py
+++ aop_sale/models/sale_order_line.py
@@ -23,26 +23,27 @@ class SaleOrderLine(models.Model):
         if self._context and self._context.get('copy_a_product') and self._context['order_id']:
 
             order = self.env['sale.order'].browse(self._context['order_id'])
-            line = order.order_line[-1]
+            if order.order_line:
+                line = order.order_line[-1]
 
-            value = {'name': line.name,
-             'product_id': line.product_id.id,
-             'service_product_id': line.service_product_id.id,
-             'spare_part_ids': [(6, 0, line.spare_part_ids.ids)],
-             'route_id': line.route_id.id,
-             'product_uom_qty': line.product_uom_qty,
-             'price_unit': line.price_unit,
-             'price_subtotal': line.price_subtotal,
-             'tax_id': [(6, 0, line.tax_id.ids)],
-             'qty_delivered': line.qty_delivered,
-             'qty_invoiced': line.qty_invoiced,
-             'product_uom': line.product_uom.id,
-             'discount': line.discount,
-             'price_total': line.price_total,
+                value = {'name': line.name,
+                 'product_id': line.product_id.id,
+                 'service_product_id': line.service_product_id.id,
+                 'spare_part_ids': [(6, 0, line.spare_part_ids.ids)],
+                 'route_id': line.route_id.id,
+                 'product_uom_qty': line.product_uom_qty,
+                 'price_unit': line.price_unit,
+                 'price_subtotal': line.price_subtotal,
+                 'tax_id': [(6, 0, line.tax_id.ids)],
+                 'qty_delivered': line.qty_delivered,
+                 'qty_invoiced': line.qty_invoiced,
+                 'product_uom': line.product_uom.id,
+                 'discount': line.discount,
+                 'price_total': line.price_total,
 
-             }
+                 }
 
-            res.update(value)
+                res.update(value)
 
         return  res
 
